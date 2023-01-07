4b9892d32a1d551fed405d1bb290fefca7d69b43
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2023-01-07 10:53:11 -0800

    Pixel de Facebook

--- resources/views/layouts/guest-layout.blade.php
+++ resources/views/layouts/guest-layout.blade.php
@@ -1,10 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+    <!-- Meta Pixel Code -->
+    <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '340530300820909');
+    fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=340530300820909&ev=PageView&noscript=1"
+    /></noscript>
+    <!-- End Meta Pixel Code -->
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jonathan Velazquez - Diseño de Páginas Web en Tijuana B.C.</title>
+
+    <!-- Necesitamos los meta description y meta keywords para SEO. -->
+    <meta name="description" content="Diseño de páginas web en Tijuana B.C.">
+    <meta name="keywords" content="Páginas Web, Diseño de Páginas Web, Páginas Web en Tijuana, Páginas Web en Baja California, Páginas Web en México, Páginas Web en Tijuana B.C., Sitios Web, Diseño de Sitios Web, Aplicaciones, Apps, Tijuana, Jonathan Velazquez, Jonathan V, Jonathan V.">
 
     <!-- CSS -->
     <link rel="stylesheet" href="{{ asset('css/normalize.css') }}">
