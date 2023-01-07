47cb45505cd97db34d09e203ca790f02455b266c
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2023-01-07 10:57:34 -0800

    Google Analytics

--- resources/views/layouts/guest-layout.blade.php
+++ resources/views/layouts/guest-layout.blade.php
@@ -20,6 +20,18 @@
     /></noscript>
     <!-- End Meta Pixel Code -->
 
+    <!-- Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-10FF3S0PMQ"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-10FF3S0PMQ');
+    </script>
+    <!-- End Google Analytics -->
+
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
